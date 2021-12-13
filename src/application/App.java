e15ccd1541e984015b5251235cf4880690fc6b09
earl-stephens <stephensearl@hotmail.com>
2021-12-12 21:59:37 -0700

    Added code to main method

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,9 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("This is another line of text.");
+		System.out.println("Final line of text.");
 
 	}
 
